c5ca54232ff4ecbf68390cee7e9021815a9e11f1
Mileteas <grountish@gmail.com>
2020-01-14 04:00:21 +0100

    aa

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-let mic;
+let NUMSINES = 20; // how many of these things can we do at once?
+let sines = new Array(NUMSINES); // an array to hold all the current angles
+let rad; // an initial radius value for the central sine
+let i; // a counter variable
+
+// play with these to get a sense of what's going on:
+let fund = 0.005; // the speed of the central sine
+let ratio = 1; // what multiplier for speed is each additional sine?
+let alpha = 50; // how opaque is the tracing system
+
+let trace = false; // are we tracing?
 
 function setup() {
-  createCanvas(710, 200);
+  createCanvas(710, 400);
 
-  // Create an Audio input
-  mic = new p5.AudioIn();
+  rad = height / 4; // compute radius for central circle
+  background(204); // clear the screen
 
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
-  mic.start();
+  for (let i = 0; i<sines.length; i++) {
+    sines[i] = PI; // start EVERYBODY facing NORTH
+  }
 }
 
 function draw() {
-  background(200);
+  if (!trace) {
+    background(204); // clear screen if showing geometry
+    stroke(0, 255); // black pen
+    noFill(); // don't fill
+  }
 
-  // Get the overall volume (between 0 and 1.0)
-  let vol = (mic.getLevel() * 40);
-  fill(127);
-  stroke(0);
+  // MAIN ACTION
+  push(); // start a transformation matrix
+  translate(width / 2, height / 2); // move to middle of screen
 
-  // Draw an ellipse with height based on volume
-  let h = map(vol, 0, 1, height, .7);
-  ellipse(width / 4, h - 100, 50, 50);
+  for (let i = 0; i < sines.length; i++) {
+    let erad = 0; // radius for small "point" within circle... this is the 'pen' when tracing
+    // setup for tracing
+    if (trace) {
+      stroke(0, 0, 255 * (float(i) / sines.length), alpha); // blue
+      fill(0, 0, 255, alpha / 2); // also, um, blue
+      erad = 5.0 * (1.0 - float(i) / sines.length); // pen width will be related to which sine
+    }
+    let radius = rad / (i + 1); // radius for circle itself
+    rotate(sines[i]); // rotate circle
+    if (!trace) ellipse(0, 0, radius * 2, radius * 2); // if we're simulating, draw the sine
+    push(); // go up one level
+    translate(0, radius); // move to sine edge
+    if (!trace) ellipse(0, 0, 5, 5); // draw a little circle
+    if (trace) ellipse(0, 0, erad, erad); // draw with erad if tracing
+    pop(); // go down one level
+    translate(0, radius); // move into position for next sine
+    sines[i] = (sines[i] + (fund + (fund * i * ratio))) % TWO_PI; // update angle based on fundamental
+  }
+
+  pop(); // pop down final transformation
+
+}
+
+function keyReleased() {
+  if (key==' ') {
+    trace = !trace;
+    background(255);
+  }
 }
