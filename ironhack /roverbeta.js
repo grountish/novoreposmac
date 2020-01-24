b79e79f3d3e83ca1ce6457f2705e12e0b2c39dea
mileteas <grountish@gmail.com>
2020-01-24 03:28:54 +0100

    subiendo roverbeta y IronSkyDrive

--- ironhack /roverbeta.js
+++ ironhack /roverbeta.js
@@ -1,14 +1,44 @@
-var roover = {
+var rover1 = {
+    name: "rover1",
+    x:0,
+    y:0,
     position: [1,1],
     direction: "N",
-    travelLog: []
-  };
-  
-let direction = roover.direction;
-let position = roover.position; 
-let travel = roover.travelLog;
+    travelLog: [],
+    myTurn: true
+};
+var rover2 = {
+    name: "rover2",
+    x:0,
+    y:9,
+    position: [0,9],
+    direction: "N",
+    travelLog: [],
+    myTurn: false
+};
+let grid = [
+    ['rover1','X','','','','','','','','',],
+    ['','','','','','','','X','','',],
+    ['','','','','','','','','','',],
+    ['','','','X','','','','','','',],
+    ['','X','','','','','','X','','',],
+    ['','','','','','','','','','',],
+    ['','','','','','','','','','',],
+    ['','','','','X','','','','','',],
+    ['','','','','','','','','X','',],
+    ['rover2','','','','','','','','','',],
+]
 
-function turnLeft(roover){
+let rovers = [rover1, rover2];
+let roverArray = [rover1, rover2];
+let direction = rovers[0].direction;
+let position = rovers[0].position; 
+let travel = rovers[0].travelLog;
+
+rover = roverArray[0];
+
+
+function turnLeft(rover){
     switch(direction) {
         case 'N':
           direction = "E";
@@ -24,11 +54,11 @@ function turnLeft(roover){
           break;
     }
     
-    console.log(`the roover direction is: ${direction} `);
+    console.log(`the rover direction is: ${direction} `);
     console.log("turnLeft was called!");
 }
   
-function turnRight(roover){
+function turnRight(rover){
     switch(direction) {
         case 'N':
           direction = "W";
@@ -44,11 +74,11 @@ function turnRight(roover){
           break;
     }
     
-    console.log(`the roover direction is: ${direction} `);
+    console.log(`the rover direction is: ${direction} `);
     console.log("turnRight was called!");
 }
   
-function moveForward(roover){
+function moveForward(rover){
     if(position[0] >= 1 && position[0] <= 9 && position[1] >= 1 && position[1] <= 9){
     switch (direction) {
         case "N":
@@ -64,13 +94,13 @@ function moveForward(roover){
             position[0]++; 
             break;   
     }
-    //console.log(`the roover position is: ${position}`);
+    //console.log(`the rover position is: ${position}`);
     console.log("moveForward was called")
 } else {
     console.log("you can't move outside the map");
 }
 }
-function moveBackward(roover){
+function moveBackward(rover){
     if(position[0] >= 1 && position[0] <= 9 && position[1] >= 1 && position[1] <= 9){
         
     switch (direction) {
@@ -87,44 +117,42 @@ function moveBackward(roover){
             position[0]--; 
             break;   
     }
-    //console.log(`the roover position is: ${position}`);
     console.log("moveBackward was called");
 } else {
     console.log("you can't move outside the map");
 }
 }
-function command(roover,orders) {
+function command(rover,orders) {
     for (let i = 0; i < orders.length; i++) {
         let order = orders[i];
+
         
         if  (order !== "l" && order !== "f" && order !== "r" && order !== "b" ) {
-            //console.log;
             return ("Provide correct commands!");
+        
 
         }   else {        
         switch (order) {
             case "l":
-                turnLeft(roover);
+                turnLeft(rover);
                 break;
             case "r":
-                turnRight(roover);
+                turnRight(rover);
                 break;
             case "f":
-                moveForward(roover);
+                moveForward(rover);
                 break;
             case "b":
-                moveBackward(roover);
+                moveBackward(rover);
                 break;
         }
-        
-
-        
+    
     }
 
-console.log(`roover has position x=${position[0]}, y=${position[1]}`);
-travel.push(i-- + " the path is: " + position + " \n " );
-
+console.log(`${rover.name} has position x=${position[0]}, y=${position[1]}`);
+travel.push(i + " the path is: " + position + " \n " );
 }
 console.log(travel); 
 
 }
+
